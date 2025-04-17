3d8e176a20c68348ec7dea5d282a090f71735f11
Santidev21 <orsantiago21@gmail.com>
2025-04-17 17:23:36 -0500

    Changed GitIgnore

--- split-it-ui/src/app/app.routes.ts
+++ split-it-ui/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
       path: 'auth',
-      loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+      loadChildren: () => import('../app/modules/auth/auth.module').then(m => m.AuthModule)
     },
     {
       path: 'dashboard',
